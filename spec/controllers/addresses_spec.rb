38bee39eeca69d40242b5f73e6beea9e26e0773f
pezholio <pezholio@gmail.com>
2014-11-03 12:29:25 +0000

    Fix up test

--- spec/controllers/addresses_spec.rb
+++ spec/controllers/addresses_spec.rb
@@ -58,7 +58,7 @@ RSpec.describe AddressesController, :type => :controller do
 
       json = JSON.parse(response.body)
 
-      expect(json['addresses'].first['url']).to match /address\/[0-9]+/
+      expect(json['addresses'].first['url']).to match /addresses\/[0-9]+/
     end
   end
 
