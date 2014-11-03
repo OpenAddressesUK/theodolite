cd9154cd096539a62e273da882f177c751d47f2f
pikesley <github@orgraphone.org>
2014-11-03 15:23:29 +0000

    Multiple addresses for a postcode
    PUSH IT TO LIVE, SEE IF IT WORKS

--- spec/request/postcode_spec.rb
+++ spec/request/postcode_spec.rb
@@ -8,4 +8,17 @@ RSpec.describe 'Postcode Entry Point', type: :request do
 
     expect(response).to redirect_to("/addresses/#{address.id}.json")
   end
+
+  it 'redirects to multiple address where appropriate' do
+    1.upto(5) do |i|
+      FactoryGirl.create(:address, sao: i)
+    end
+
+    FactoryGirl.create(:address, postcode: 'SW1A 1AA')
+
+    get "/postcode/#{Address.first.postcode.to_url}.json"
+
+    json = JSON.parse(response.body)
+    expect(json['addresses'].count).to eq(5)
+  end
 end
