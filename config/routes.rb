529f7b079ef2e472850e3c88f57c191c3ddebc50
James Smith <james@floppy.org.uk>
2014-11-05 14:06:31 +0000

    change query route

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
 
   resources :addresses
 
-  get '/addresses/:town/:locality/:postcode/:street/:pao/:sao' => 'addresses#query'
-
   get '/postcode/:postcode' => 'addresses#query', defaults: {
                                 town: '-',
                                 locality: '-',
--- spec/routing/address_routes_spec.rb
+++ spec/routing/address_routes_spec.rb
@@ -3,9 +3,9 @@ require "rails_helper"
 describe "routes" do
 
   it "should route addresses queries" do
-    expect(:get => "/addresses/london/372-old-street/ec1v-9av/old-street/372/335-rosden-house").to route_to(
+    expect(:get => "/addresses?town=london&locality=372-old-street&postcode=ec1v-9av&street=old-street&pao=372&sao=335-rosden-house").to route_to(
       controller: "addresses",
-      action: "query",
+      action: "index",
       town: "london",
       locality: "372-old-street",
       postcode: "ec1v-9av",
