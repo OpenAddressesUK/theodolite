84880d642805d7fb8696e5d431251f3ad2c692b5
Jeni Tennison <jeni@theodi.org>
2014-11-16 23:22:00 +0000

    put pointers on a map

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -2,14 +2,27 @@ $(document).ready(function () {
 
 	L.mapbox.accessToken = 'pk.eyJ1Ijoib3BlbmFkZHJlc3NlcyIsImEiOiJ6NG5zdy1jIn0.3geJ_DPjwkm0iTLlV0CA3g';
 	var map = L.mapbox.map('map', 'openaddresses.k75i6o4d');
+	var oaIcon = L.icon({
+    	iconUrl: '/images/mapicon.png',
+    	iconSize: [35, 35],
+    	iconAnchor: [33, 35],
+    	popupAnchor: [0, 35]
+	});
 
 	var locations = [];
 
 	$('*[data-postcode]').each(function () {
-		var postcode = $(this).attr('data-postcode').replace(/ /, '');
-		$.getJSON('http://uk-postcodes.com/postcode/' + postcode + '.json?nocache=' + new Date(), function (data) {
-			console.log(postcode);
-			console.log(data);
+		var $address = $(this);
+		var postcode = $address.attr('data-postcode').replace(/ /, '');
+		$.getJSON('http://uk-postcodes.com/postcode/' + postcode + '.json', function (data) {
+			var geo = data.geo;
+			var lat = geo.lat;
+			var long = geo.lng;
+			if (!(lat === undefined || long === undefined)) {
+				var marker = L.marker([lat, long], { icon: oaIcon, riseOnHover: true }).addTo(map);
+				locations.push([lat, long]);
+				map.fitBounds(L.latLngBounds(locations));
+			}
 		});
 	});
 
