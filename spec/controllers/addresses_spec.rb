7eaf337a290a7beff328ed0a348b50e047a176ff
pezholio <pezholio@gmail.com>
2014-11-18 17:07:21 +0000

    Fix tests

--- spec/controllers/addresses_spec.rb
+++ spec/controllers/addresses_spec.rb
@@ -5,8 +5,8 @@ RSpec.describe AddressesController, :type => :controller do
     render_views
 
     it 'responds successfully' do
-      25.times do
-        FactoryGirl.create(:address)
+      25.times do |i|
+        FactoryGirl.create(:address, pao: i)
       end
       get :index, format: :json
 
@@ -15,8 +15,8 @@ RSpec.describe AddressesController, :type => :controller do
     end
 
     it 'shows the correct amount of adresses' do
-      25.times do
-        FactoryGirl.create(:address)
+      25.times do |i|
+        FactoryGirl.create(:address, pao: i)
       end
 
       get :index, format: :json
@@ -27,8 +27,8 @@ RSpec.describe AddressesController, :type => :controller do
     end
 
     it 'shows the correct pagination information' do
-      30.times do
-        FactoryGirl.create(:address)
+      30.times do |i|
+        FactoryGirl.create(:address, pao: i)
       end
 
       get :index, format: :json
@@ -38,8 +38,8 @@ RSpec.describe AddressesController, :type => :controller do
     end
 
     it 'shows the correct data when page is specified' do
-      30.times do
-        FactoryGirl.create(:address)
+      30.times do |i|
+        FactoryGirl.create(:address, pao: i)
       end
 
       get :index, page: 2, format: :json
@@ -51,8 +51,8 @@ RSpec.describe AddressesController, :type => :controller do
     end
 
     it 'shows the correct pagination information when per_page is specified' do
-      30.times do
-        FactoryGirl.create(:address)
+      30.times do |i|
+        FactoryGirl.create(:address, pao: i)
       end
 
       get :index, page: 2, per_page: 4, format: :json
@@ -61,13 +61,13 @@ RSpec.describe AddressesController, :type => :controller do
 
     it 'shows the url for addresses' do
       FactoryGirl.create(:address)
-      FactoryGirl.create(:address)
+      FactoryGirl.create(:address, pao: "456")
 
       get :index, format: :json
 
       json = JSON.parse(response.body)
 
-      expect(json['addresses'].first['url']).to match /http:\/\/test\.host\/addresses\/[0-9]+/
+      expect(json['addresses'].first['url']).to match /http:\/\/test\.host\/addresses\/[0-9a-z]+/i
     end
   end
 
@@ -75,10 +75,10 @@ RSpec.describe AddressesController, :type => :controller do
     render_views
 
     before :each do
-      @postcode = "W12 8QT"
-      @town = "Gotham City"
-      @locality = "Arkham"
-      @street = "Bat Street"
+      @postcode = FactoryGirl.create(:postcode, name: "W12 8QT")
+      @town = FactoryGirl.create(:town, name: "Gotham City")
+      @locality = FactoryGirl.create(:locality, name: "Arkham")
+      @street = FactoryGirl.create(:street, name: "Bat Street")
       @pao = "Stately Wayne Manor"
       @sao = "The Batcave"
 
@@ -99,8 +99,6 @@ RSpec.describe AddressesController, :type => :controller do
     end
 
     it 'responds with JSON' do
-      address = FactoryGirl.create(:address, sao: @sao, pao: @pao, street: @street, locality: @locality, postcode: @postcode)
-
       get :show, id: @address.id,
                  format: :json
 
@@ -108,8 +106,6 @@ RSpec.describe AddressesController, :type => :controller do
     end
 
     it 'responds with the correct JSON' do
-      address = FactoryGirl.create(:address, sao: @sao, pao: @pao, street: @street, locality: @locality, postcode: @postcode)
-
       get :show, id: @address.id,
                  format: :json
 
@@ -117,10 +113,9 @@ RSpec.describe AddressesController, :type => :controller do
 
       expect(json['address']['sao']).to eq(@sao)
       expect(json['address']['pao']).to eq(@pao)
-      expect(json['address']['street']).to eq(@street)
-      expect(json['address']['locality']).to eq(@locality)
-      expect(json['address']['postcode']).to eq(@postcode)
-
+      expect(json['address']['street']).to eq(@street.name)
+      expect(json['address']['locality']).to eq(@locality.name)
+      expect(json['address']['postcode']).to eq(@postcode.name)
     end
 
   end
@@ -129,10 +124,10 @@ RSpec.describe AddressesController, :type => :controller do
     render_views
 
     before :each do
-      @postcode = "W12 8QT"
-      @town = "Gotham City"
-      @locality = "Arkham"
-      @street = "Bat Street"
+      @postcode = FactoryGirl.create(:postcode, name: "W12 8QT")
+      @town = FactoryGirl.create(:town, name: "Gotham City")
+      @locality = FactoryGirl.create(:locality, name: "Arkham")
+      @street = FactoryGirl.create(:street, name: "Bat Street")
       @pao = "Stately Wayne Manor"
       @sao = "The Batcave"
 
@@ -141,12 +136,13 @@ RSpec.describe AddressesController, :type => :controller do
 
     it 'redirects successfully' do
       get :index,
-          town: @town.to_url,
-          locality: @locality.to_url,
-          postcode: @postcode.to_url,
-          street: @street.to_url,
-          pao: @pao.to_url,
-          sao: @sao.to_url
+          town: @town.name,
+          locality: @locality.name,
+          postcode: @postcode.name,
+          street: @street.name,
+          pao: @pao,
+          sao: @sao,
+          format: :json
 
       expect(response).to be_redirect
       expect(response).to have_http_status(307)
@@ -154,24 +150,25 @@ RSpec.describe AddressesController, :type => :controller do
 
     it 'assigns correctly' do
       get :index,
-          town: @town.to_url,
-          locality: @locality.to_url,
-          postcode: @postcode.to_url,
-          street: @street.to_url,
-          pao: @pao.to_url,
-          sao: @sao.to_url
+          town: @town.name,
+          locality: @locality.name,
+          postcode: @postcode.name,
+          street: @street.name,
+          pao: @pao,
+          sao: @sao,
+          format: :json
 
       expect(assigns(:addresses).first).to eq (@address)
     end
 
     it 'retains the format of the original request' do
       get :index,
-          town: @town.to_url,
-          locality: @locality.to_url,
-          postcode: @postcode.to_url,
-          street: @street.to_url,
-          pao: @pao.to_url,
-          sao: @sao.to_url,
+          town: @town.name,
+          locality: @locality.name,
+          postcode: @postcode.name,
+          street: @street.name,
+          pao: @pao,
+          sao: @sao,
           format: :json
 
       expect(response.header["Location"]).to match /\.json/
@@ -180,44 +177,49 @@ RSpec.describe AddressesController, :type => :controller do
     it 'returns empty list if no addresses are found' do
       get :index,
           town: 'fake-town',
-          locality: @locality.to_url,
-          postcode: @postcode.to_url,
-          street: @street.to_url,
-          pao: @pao.to_url,
-          sao: @sao.to_url,
+          locality: @locality.name,
+          postcode: @postcode.name,
+          street: @street.name,
+          pao: @pao,
+          sao: @sao,
           format: :json
-          
+
       json = JSON.parse(response.body)
       expect(json['addresses'].count).to eq(0)
     end
 
     it 'redirects if parameters are missing' do
       get :index,
-          town: @town.to_url,
+          town: @town.name,
           #locality: nil,
-          postcode: @postcode.to_url,
-          street: @street.to_url,
-          pao: @pao.to_url,
-          sao: @sao.to_url,
+          postcode: @postcode.name,
+          street: @street.name,
+          pao: @pao,
+          sao: @sao,
           format: :json
 
-      expect(response).to redirect_to("/addresses/#{@address.id}.json")
+      expect(response).to redirect_to("/addresses/#{@address.token}.json")
     end
 
     context 'with multiple results' do
 
       it 'lists all possible results if parameters are missing' do
         1.upto(4) do |i|
-          FactoryGirl.create(:address, sao: i, pao: @pao, street: @street, locality: @locality, postcode: @postcode)
+          FactoryGirl.create(:address,
+                            sao: i,
+                            pao: @pao,
+                            street: @street,
+                            town: @town,
+                            locality: @locality,
+                            postcode: @postcode
+                            )
         end
 
         get :index,
-            town: @town.to_url,
-            locality: @locality.to_url,
-            postcode: @postcode.to_url,
-            street: @street.to_url,
-            pao: @pao.to_url,
-            #sao: nil,
+            town: @town.name,
+            locality: @locality.name,
+            postcode: @postcode.name,
+            street: @street.name,
             format: :json
 
         json = JSON.parse(response.body)
@@ -229,15 +231,15 @@ RSpec.describe AddressesController, :type => :controller do
 
     it 'redirects on non-URL-encoded strings' do
       get :index,
-          town: @town,
-          locality: @locality,
-          postcode: @postcode,
-          street: @street,
+          town: @town.name,
+          locality: @locality.name,
+          postcode: @postcode.name,
+          street: @street.name,
           pao: @pao,
           sao: @sao,
           format: :json
 
-      expect(response).to redirect_to("/addresses/#{@address.id}.json")
+      expect(response).to redirect_to("/addresses/#{@address.token}.json")
     end
   end
 end
--- spec/request/postcode_spec.rb
+++ spec/request/postcode_spec.rb
@@ -4,9 +4,9 @@ RSpec.describe 'Postcode Entry Point', type: :request do
   it 'redirects to a single address where appropriate' do
     address = FactoryGirl.create(:address)
 
-    get "/postcode/#{address.postcode.to_url}.json"
+    get "/postcode/#{address.postcode.name.to_url}.json"
 
-    expect(response).to redirect_to("/addresses/#{address.id}.json")
+    expect(response).to redirect_to("/addresses/#{address.token}.json")
   end
 
   it 'redirects to multiple address where appropriate' do
@@ -14,19 +14,19 @@ RSpec.describe 'Postcode Entry Point', type: :request do
       FactoryGirl.create(:address, sao: i)
     end
 
-    FactoryGirl.create(:address, postcode: 'SW1A 1AA')
+    FactoryGirl.create(:address, postcode: FactoryGirl.create(:postcode, name: 'SW1A 1AA'))
 
-    get "/postcode/#{Address.first.postcode.to_url}.json"
+    get "/postcode/#{Address.first.postcode.name.to_url}.json"
 
     json = JSON.parse(response.body)
     expect(json['addresses'].count).to eq(5)
   end
 
   it 'supports non-url encoded postcodes' do
-    address = FactoryGirl.create(:address, postcode: 'SW1A 1AA')
+    address = FactoryGirl.create(:address, postcode: FactoryGirl.create(:postcode, name: 'SW1A 1AA'))
 
     get "/postcode/SW1A%201AA.json"
 
-    expect(response).to redirect_to("/addresses/#{address.id}.json")
+    expect(response).to redirect_to("/addresses/#{address.token}.json")
   end
 end
