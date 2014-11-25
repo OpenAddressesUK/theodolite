acc83191003ed0bfad6061c2bd6ac2c205045d03
pezholio <pezholio@gmail.com>
2014-11-25 12:48:35 +0000

    Add routes for address parts

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
   root to: 'addresses#index'
 
   resources :addresses
+  resources :streets, only: [:show]
+  resources :localities, only: [:show]
+  resources :towns, only: [:show]
+  resources :streets, only: [:show]
+  resources :postcodes, only: [:show]
 
   get '/postcode/:postcode' => 'addresses#index'
 
