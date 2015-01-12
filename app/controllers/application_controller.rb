5bc249204d93b76598774fecbbda2a2867a24568
pezholio <pezholio@gmail.com>
2015-01-12 07:11:49 +0000

    And route to download torrent, rather than acessing directly

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,19 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  def get_torrent
+    if params[:file]
+      redirect_to "/#{params[:file]}"
+    else
+      raise ActionController::RoutingError.new('Not Found')
+    end
+  end
+
   private
 
     def pagination
       @page = (params[:page] || 1).to_i
       @per_page = (params[:per_page] || 25).to_i
     end
-    
+
 end
