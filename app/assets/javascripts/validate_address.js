3b853a56d196a6ad0998f46503fcabe98cb53f7c
James Smith <james@floppy.org.uk>
2015-02-19 07:42:22 +0000

    validation javascript and UI

---
+++ app/assets/javascripts/validate_address.js
@@ -0,0 +1,30 @@
+function validateAddress(url, exists, attribution, reason, callback) {
+
+  $.getJSON(url+".json").done(function( data ) {      
+      derivations = data['address']['provenance']['activity']['derived_from']
+      for (derived in derivations) {
+          url = derivations[derived]['urls'][0]          
+          if (url.indexOf("ernest") > 0) {
+              validation_url = url + "/validations";
+              data = {
+                  exists: exists,
+                  attribution: attribution,
+                  reason: reason
+              };
+              $.ajax(validation_url, {
+                  type: "POST",
+                  contentType : 'application/json',
+                  data: JSON.stringify(data),
+                  success: function() {
+                    callback(true);
+                  },
+                  error: function() {
+                    callback(false);
+                  }
+              });
+          }
+      }
+  });
+
+  
+}
