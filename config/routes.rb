908a30934c3925b1194c868fb9f8132ae80d5de0
James Smith <james@floppy.org.uk>
2015-02-21 13:56:04 +0000

    needs routes!

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,13 @@ Rails.application.routes.draw do
 
   resources :addresses
 
+  get '/streets' => "address_parts#streets", as: "streets"
   get '/streets/:id' => "address_parts#street", as: "street"
+  get '/localities' => "address_parts#localities", as: "localities"
   get '/localities/:id' => "address_parts#locality", as: "locality"
+  get '/towns' => "address_parts#towns", as: "towns"
   get '/towns/:id' => "address_parts#town", as: "town"
+  get '/postcodes' => "address_parts#postcodes", as: "postcodes"
   get '/postcodes/:id' => "address_parts#postcode", as: "postcode"
 
   get '/postcode/:postcode' => 'addresses#index'
