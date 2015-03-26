f7674b1ee76803124e8a00a80289fdafd888acfa
Andrew Wilkinson <andy@errorstudio.co.uk>
2015-03-26 17:20:54 +0000

    Homepage redesign.

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -12,4 +12,10 @@ $(function() {
 		e.stopPropagation();
 	})
 
+	var homeGFX = $('.home-gfx')[0];;
+
+	window.onscroll = function(e) {
+			homeGFX.style.transform = 'translate3d(0px,' + Math.round(window.scrollY/6) + 'px, 0px)';
+	}
+
 });
