7560f4e65edcd1b3b2cf1581f6ff2d2c6f514063
pikesley <github@orgraphone.org>
2015-03-31 12:04:41 +0100

    rebuild_from_parts

--- lib/address_fixer.rb
+++ lib/address_fixer.rb
@@ -1,17 +1,15 @@
 module AddressFixer
-  
+
   def self.fix_lat_lng(a)
-    result = false
+    changed = false
     ["postcode", "locality", "street"].each do |type|
       lat_lng = a.send(type).lat_lng rescue nil
       if !lat_lng.nil? && lat_lng.first > 4
-        result = true
-        a.send(type).lat_lng = [lat_lng[1], lat_lng[0]]
+        changed = true
       end
     end
-    a.save(validate: false)
-    result
+
+    a.rebuild_from_parts! if changed
+    changed
   end
-
 end
-
