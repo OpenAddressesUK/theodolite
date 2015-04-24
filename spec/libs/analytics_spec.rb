df42e607fb79542e00a42da88815818d55dbbff4
pezholio <pezholio@gmail.com>
2015-04-24 13:55:56 +0100

    Add tests for Sorting Office

--- spec/libs/analytics_spec.rb
+++ spec/libs/analytics_spec.rb
@@ -13,4 +13,14 @@ describe Analytics do
     end
   end
 
+  it 'gets analytics for sorting office' do |example|
+    Timecop.freeze(Date.new(2015, 04, 23)) do
+      VCR.use_cassette(example.metadata[:full_description]) do
+        a = Analytics.new(property: 'Sorting Office', path: '\/address')
+
+        expect(a.result).to eq(3000)
+      end
+    end
+  end
+
 end
