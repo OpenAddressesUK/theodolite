7cfaef069c853217606f4e8777febeace72aeef4
pezholio <pezholio@gmail.com>
2015-05-08 15:24:18 +0100

    Add custom devise controller

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,7 @@
+class Users::RegistrationsController < Devise::RegistrationsController
+
+  def edit
+
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  devise_for :users, controllers: { registrations: "users/registrations" }
   get '/addresses/download' => "addresses#download"
 
   resources :addresses
