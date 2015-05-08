cd8367478b60f755ac0eb83bf7ddcd26d85c1adf
pezholio <pezholio@gmail.com>
2015-05-08 15:47:59 +0100

    Redirect to edit after signin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  def after_sign_in_path_for(resource)
+    edit_user_registration_path
+  end
+
   def get_torrent
     if params[:file]
       redirect_to "/#{params[:file]}"
